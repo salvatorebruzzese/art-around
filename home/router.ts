26a7013f2dfd7527a5415de99726fd935c4c20b5
Salvatore Bruzzese <salvatorebruzzese05@gmail.com>
2026-08-07 09:28:33 +0200

    add(frontend): new palette, home, login and signup pages

--- home/router.ts
+++ home/router.ts
@@ -8,4 +8,12 @@ router.get('/', (req, res) => {
   res.sendFile(join(rootDir, 'home/index.html'))
 })
 
+router.get('/login', (req, res) =>
+  res.sendFile(join(rootDir, 'home/login.html')),
+)
+
+router.get('/signup', (req, res) =>
+  res.sendFile(join(rootDir, 'home/signup.html')),
+)
+
 export default router
