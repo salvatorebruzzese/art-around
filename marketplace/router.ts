eb56a05218a55e28c6b00ab258898b64a6984369
Salvatore Bruzzese <salvatorebruzzese05@gmail.com>
2026-08-07 12:08:15 +0200

    add(frontend): marketplace draft and new link in home

---
+++ marketplace/router.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+import { join } from 'path'
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  res.sendFile(join(rootDir, 'marketplace/index.html'))
+})
+
+export default router
--- marketplace/vite.config.js
+++ marketplace/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: 'index.html',
-     },
+      },
     },
   },
 })
