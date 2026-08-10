124465acf1bf5c56e632adfe0cd3b8fd484b898e
Salvatore Bruzzese <salvatorebruzzese05@gmail.com>
2026-08-10 10:02:25 +0200

    refactor/fix: finally managed to set up tailwind outside of html files

--- home/router.ts
+++ home/router.ts
@@ -1,10 +1,13 @@
 import express from 'express'
 import { join } from 'path'
+import rootDir from '../backend/rootdir.js'
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
   res.sendFile(join(rootDir, 'home/index.html'))
 })
+
+router.use('/', express.static(join(rootDir, '/home/')))
 
 export default router
--- home/vite.config.js
+++ home/vite.config.js
@@ -1,7 +1,9 @@
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/home/',
+  plugins: tailwindcss(),
   build: {
     outDir: '../dist/home/',
     rollupOptions: {
