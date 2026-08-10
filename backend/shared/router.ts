1589807356d7247df5cdf9dd8c294cdb936f1643
William Brusa <william.brusa@studio.unibo.it>
2026-08-10 14:22:23 +0200

    add(router): shared error handler for Express responses
    Introduce handleLeft function to recycle error response handling

---
+++ backend/shared/router.ts
@@ -0,0 +1,20 @@
+import { Response } from 'express'
+import { DBError, NotFound, AccessDenied } from './errors.js'
+import { assertNever } from './utils.js'
+
+type KnownError = DBError | NotFound | AccessDenied
+
+export function handleLeft(res: Response) {
+  return (error: KnownError) => {
+    switch (error.type) {
+      case 'DBError':
+        return res.status(500).json({ error })
+      case 'NotFound':
+        return res.status(404).json({ error })
+      case 'AccessDenied':
+        return res.status(403).json({ error })
+      default:
+        assertNever(error)
+    }
+  }
+}
