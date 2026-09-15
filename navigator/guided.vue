d261dad185006dbc726aede013908ec13686ccea
Salvatore Bruzzese <salvatorebruzzese05@gmail.com>
2026-09-15 16:30:44 +0200

    refactor(guided): putting guided into a vue file

---
+++ navigator/guided.vue
@@ -0,0 +1,24 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template
+  class="h-screen overflow-hidden flex flex-col items-center justify-center"
+>
+  <div
+    class="flex flex-col justify-center items-center rounded-xl shadow-md shadow-p-soft py-4 px-8 gap-4 text-center max-w-md mx-4"
+  >
+    <h1 class="text-3xl text-p-medium">Inserisci il codice</h1>
+    <label class="border border-p-soft rounded-sm">
+      <input
+        class="text-p-dark rounded-sm"
+        type="text"
+        autocomplete="one-time-code"
+        inputmode="text"
+        required
+      />
+    </label>
+  </div>
+  <a
+    href="/navigator/"
+    class="block text-center mt-6 text-p-medium hover:text-p-dark hover:underline text-md"
+    >Torna indietro</a
+  >
+</template>
